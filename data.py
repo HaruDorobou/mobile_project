0ee1b5cf8be06c5e6b97330c4f487451580efd3a
HaruDorobou <suk05088@gmail.com>
2021-11-09 18:06:12 +0900

    Final

--- data.py
+++ data.py
@@ -1,41 +1,74 @@
 import sqlite3
 import numpy as np
 import pandas as pd
+from xml.etree.ElementTree import parse
+
 
 class Data:
 
     # db => 'database_example.db'
-    def make_conn(self, db):
+    def data_extract_db(self, db, sql):
         conn = sqlite3.connect(db)
         cur = conn.cursor()
-        return cur
 
-    def exec_sql(self, cur, sql):
         cur.execute(sql)
         cols = [column[0] for column in cur.description]
         data = np.array([cols])
-        print(data)
+        # print(data)
 
         rows = cur.fetchall()
         rows = np.array(rows)
-        print(rows)
+        record_len = str(len(rows))
+        # print(rows)
+
+        data = np.append(data, rows, axis=0)
+        # print('data_extract_db(' + db + ', ' + sql + ')')
+        # print(data)
+
+        conn.close()
+        return data, record_len
+
+    def parsing_buildprop(self, file):
+        s1, s2, s3, s4 = '', '', '', ''
+        os, model, build_id = '', '', ''
+        f = open(file, 'r', encoding='utf-8')
+
+        fs = f.readlines()
+
+        for line in fs:
+            line = line[:-1].split('=', 2)
+            if line[0] == 'net.bt.name':
+                s1 = line[1]
+            if line[0] == 'ro.build.version.release':
+                s2 = line[1]
+            if line[0] == 'ro.build.product':
+                s3 = line[1]
+            if line[0] == 'ro.system.build.id':
+                s4 = line[1]
+
+        if not s1 == '':
+            os = s1
+            if not s2 == '':
+                os = os + s2
+        if not s3 == '':
+            model = s3
+        if not s4 == '':
+            build_id = s4
+        return os, model, build_id
+
+    def parsing_wifi_xml(self, file):
+        tree = parse('./data/WifiConfigStore.xml')
+        root = tree.getroot()
+
+        cols = [['Wifi-ID', 'CreationTime']]
+        row = []
+        data = np.array(cols)
+        for string in root.iter('string'):
+            if string.attrib['name'] == 'SSID':
+                row = np.array([string.text[1:-1]])
+            if string.attrib['name'] == 'CreationTime':
+                row = np.append(row, np.array([string.text[5:]]))
+                data = np.append(data, [row], axis=0)
+
         return data
-
-    def parsing_buildprop(self, db):
-        pass
-
-    def parsing_contacts(self, db):
-        pass
-
-    def parsing_calllog(self, db):
-        pass
-
-    def parsing_mms(self, db):
-        pass
-
-    def parsing_calendar(self, db):
-        pass
-
-
-
 
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from function import pdf_function
+from function import function
+import data
 
 if __name__ == "__main__":
-    func_obj = pdf_function()
-    pdf = func_obj.init_pdf() # automatic add_page
+    func_obj = function()
+    pdf = func_obj.init_pdf()
+    data_control = data.Data()
 
     # Setting Init Font Set
     pdf.add_font('NanumGothic', fname='NanumGothic.ttf', uni=True)
@@ -13,89 +15,120 @@ if __name__ == "__main__":
     func_obj.set_title(pdf, '디지털 포렌식 분석 보고서')
     pdf.ln(30)
 
-    func_obj.set_report_date(pdf) # 보고서 작성 일시
+    func_obj.set_report_date(pdf)  # 보고서 작성 일시
     pdf.ln(15)
 
-    func_obj.set_where(pdf) # 보고서 작성 장소
+    func_obj.set_where(pdf)  # 보고서 작성 장소
     pdf.ln(15)
 
-    func_obj.set_target(pdf) # 보고서 의뢰 대상
+    func_obj.set_target(pdf)  # 보고서 의뢰 대상
     pdf.ln(15)
 
-    func_obj.set_organ(pdf) # 보고서 의뢰 기관
+    func_obj.set_organ(pdf)  # 보고서 의뢰 기관
     pdf.ln(15)
 
-    func_obj.set_timezone(pdf) # 기기의 Timezone
+    func_obj.set_modelname(pdf)  # 기기의 이름
     pdf.ln(15)
 
-    func_obj.set_os(pdf) # 기기의 OS
+    func_obj.set_os(pdf)  # 기기의 OS
     pdf.ln(15)
 
-    func_obj.set_buildID(pdf) # 기기의 추출당시 Build ID
+    func_obj.set_buildID(pdf)  # 기기의 추출당시 Build ID
+    pdf.ln(15)
+
+    func_obj.set_timezone(pdf)  # 기기의 Timezone
     pdf.ln(15)
 
     # 보고서 첫 페이지 내용 끝 #
     # 분석내용 시작
-    d_data = (
-        ("col1", "col2", "col3", "col4"),
-        ("2015-01-23", "Caron", "34", "San Juan"),
-        ("2012-05-16", "Ramos", "45", "Orlando"),
-        ("2021-11-03", "Banks", "19", "Los Angeles"),
-        ("2020-12-22", "Cimon", "31", "Saint-Mahturin-sur-Loire"),
-        ("2019-07-30", "Garfunkle", "31", "Saint-Mahturin-sur-Loire"),
-        ("2007-08-19", "Yamato", "31", "Saint-Mahturin-sur-Loire"),
-        ("2006-02-07", "Akagi", "31", "Saint-Mahturin-sur-Loire")
-    ) # example data
 
     pdf.set_font('NanumGothic', size=26)
     pdf.write(txt='분석내용')
     pdf.ln(15)
 
     # Contacts
+    sql = 'select raw_contacts._id as id, raw_contacts.display_name as name, \
+        phone_lookup.normalized_number as number \
+        from raw_contacts inner join phone_lookup on \
+        raw_contacts._id = phone_lookup.raw_contact_id \
+        order by raw_contacts._id'
+    data, record_num = data_control.data_extract_db('./data/contacts2.db', sql)
+
     pdf.set_font('NanumGothic', size=18)
-    pdf.write(txt='연락처 합계 : ' + 'number')
+    pdf.write(txt='연락처 합계 : ' + record_num)
     pdf.ln(10)
 
     pdf.set_font('NanumGothic', size=10)
     pdf.write(txt='연락처 목록')
     pdf.ln(5)
-    func_obj.make_table(pdf, data=d_data) # table result
+    func_obj.make_table(pdf, data=data)  # table result
     pdf.ln(15)
 
     # Calendar
+    sql = 'select title as event, description as memo, dtstart as date_start, dtend as date_end \
+        from Events where calendar_id = 1 order by date_start'
+    data, record_num = data_control.data_extract_db('./data/calendar.db', sql)
+
     pdf.set_font('NanumGothic', size=18)
-    pdf.write(txt='캘린더 이벤트 합계 : ' + 'number')
+    pdf.write(txt='캘린더 이벤트 합계 : ' + record_num)
     pdf.ln(10)
 
     pdf.set_font('NanumGothic', size=10)
     pdf.write(txt='캘린더 이력')
     pdf.ln(5)
-    func_obj.make_table(pdf, data=d_data) # table result
+    func_obj.make_table(pdf, data=data)  # table result
     pdf.ln(15)
 
     # MMS
+    sql = 'select date, address, body as message_body from sms \
+        order by date'
+    data, record_num = data_control.data_extract_db('./data/mmssms.db', sql)
+
     pdf.set_font('NanumGothic', size=18)
-    pdf.write(txt='문자메시지 합계 : ' + 'number')
+    pdf.write(txt='문자메시지 합계 : ' + record_num)
     pdf.ln(10)
 
     pdf.set_font('NanumGothic', size=10)
     pdf.write(txt='문자메시지 이력')
     pdf.ln(5)
-    func_obj.make_table(pdf, data=d_data) # table result
+    func_obj.make_table(pdf, data=data)  # table result
     pdf.ln(15)
 
     # Call Log
+    sql = 'select date, number, name from calls \
+        order by date'
+    data, record_num = data_control.data_extract_db('./data/calllog.db', sql)
+
     pdf.set_font('NanumGothic', size=18)
-    pdf.write(txt='문자메시지 합계 : ' + 'number')
+    pdf.write(txt='전화 이력 합계 : ' + record_num)
     pdf.ln(10)
 
     pdf.set_font('NanumGothic', size=10)
     pdf.write(txt='전화 이력')
     pdf.ln(5)
-    func_obj.make_table(pdf, data=d_data) # table result
+    func_obj.make_table(pdf, data=data)  # table result
+    pdf.ln(15)
+
+    # 연동 계정
+    pdf.set_font('NanumGothic', size=18)
+    pdf.write(txt='연동 계정')
+    pdf.ln(10)
+    pdf.set_font('NanumGothic', size=10)
+    sql = 'select _id as id, account_name from accounts'
+    data, _ = data_control.data_extract_db('./data/accounts.notifications.db', sql)
+    func_obj.make_table(pdf, data=data)  # table result
+    pdf.ln(15)
+
+    # wifi 연결 이력
+    pdf.set_font('NanumGothic', size=18)
+    pdf.write(txt='wifi 연결 이력')
+    pdf.ln(10)
+    pdf.set_font('NanumGothic', size=10)
+    data = data_control.parsing_wifi_xml('./data/WifiConfigStore.xml')
+    func_obj.make_table(pdf, data=data)  # table result
     pdf.ln(15)
 
     # 분석내용 끝
-    
+
     # 보고서 저장
     pdf.output("result.pdf")
